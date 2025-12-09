a00979fba1f929ff00f0188ce45944cae7939ec3
Ejiwumi Basir Abiodun <haryomidey15@gmail.com>
2025-12-09 20:44:58 +0100

    mappicker updated

--- components/admin/MapPicker.tsx
+++ components/admin/MapPicker.tsx
@@ -1,17 +1,20 @@
-"use client"; // Important! Must be a client component
+"use client";
 
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// Fix default marker icons
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
+// FIX: Set default marker icon (NO delete _getIconUrl)
+const icon = L.icon({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
+
+L.Marker.prototype.options.icon = icon;
 
 interface MapPickerProps {
   value?: { lat: number; lng: number };
